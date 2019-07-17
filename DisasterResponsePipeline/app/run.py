a51f64362cd2465e0f85f2265072c5792446d2de
Yanxiang Yu <yueureka@gmail.com>
2019-07-17 18:56:38 -0500

    Fixed file path name

--- DisasterResponsePipeline/app/run.py
+++ DisasterResponsePipeline/app/run.py
@@ -26,11 +26,12 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+#engine = create_engine('sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///{}'.format('data/DisasterResponse.db'))
+df = pd.read_sql_table('message_categories', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("/home/workspace/models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
